89f3126377c6ae0e9b37a0b97fa23e88c8d73317
Suyash Saxena <35092213+Suyash1266@users.noreply.github.com>
2020-01-17 23:51:14 +0530

    Final commit

--- BinSearch.cpp
+++ BinSearch.cpp
@@ -4,39 +4,57 @@ using namespace std;
 
 bool binarySearchReacursive(int* array, int startIndex, int lastIndex, int element)
 {
-  if(startIndex>=lastIndex)
+  if(lastIndex>=startIndex)
+  {
+    int midIndex=startIndex+(lastIndex-1)/2;
+    if(array[midIndex]==element)
+      return true;
+    if(array[midIndex]>element)
+      return binarySearchReacursive(array,startIndex,midIndex-1,element);
+    return binarySearchReacursive(array,midIndex+1,lastIndex,element);
+  }
   return false;
-  int midIndex=(startIndex+lastIndex-1)/2;
-  else if(array[midIndex]>element)
-  return binarySearchReacursive(array,startIndex,midIndex-1,element);
-  else if(array[midIndex]<element)
-  return binarySearchReacursive(array,midIndex+1,lastIndex,element);
-  else
-  return true;
 }
 
 bool binarySearchIterative(int* array, int startIndex, int lastIndex, int element)
 {
   while(startIndex<=lastIndex)
   {
-    int midIndex=(startIndex+lastIndex-1)/2;
-    
+    int midIndex=startIndex+(lastIndex-1)/2;
+    if(array[midIndex]==element)
+      return true;
+    if(array[midIndex]<element)
+      startIndex=midIndex+1;
+    else
+      lastIndex=midIndex-1;
   }
-  return true;
+  if(startIndex>lastIndex)
+    return false;
 }
 
 int main()
 {
-  int size;
+  int size;int element;
   cout<<"\nEnter the number of elements in the array\n";
   cin>>size;
-  int array[n];
+  int array[size];
   cout<<"\nEnter the elements\n";
   for(int i=0;i<size;i++)
   {
     cin>>array[i];
   }
-  sort(array,array+n);
-
+  sort(array,array+size);
+  cout<<"\nEnter the element to be searched\n";
+  cin>>element;
+  cout<<"\nThrough Itreation\n";
+  if(binarySearchIterative(array,0,size-1,element))
+    cout<<"\nNumber Found\n";
+  else
+    cout<<"\nNumber not found\n";
+  cout<<"\nThrough recursion\n";
+  if(binarySearchReacursive(array,0,size-1,element))
+    cout<<"\nNumber Found\n";
+  else
+    cout<<"\nNumber not found\n";
   return 0;
 }
