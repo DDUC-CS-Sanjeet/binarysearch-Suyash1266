cd3843d06c3a55733d75b3b1089d628febbd29f7
Suyash Saxena <suyashlmp123@gmail.com>
2020-01-17 17:00:39 +0530

    Incomplete|Uploaded from lab

--- BinSearch.cpp
+++ BinSearch.cpp
@@ -1,20 +1,42 @@
+#include<algorithm>
 #include<iostream>
 using namespace std;
 
 bool binarySearchReacursive(int* array, int startIndex, int lastIndex, int element)
 {
+  if(startIndex>=lastIndex)
+  return false;
+  int midIndex=(startIndex+lastIndex-1)/2;
+  else if(array[midIndex]>element)
+  return binarySearchReacursive(array,startIndex,midIndex-1,element);
+  else if(array[midIndex]<element)
+  return binarySearchReacursive(array,midIndex+1,lastIndex,element);
+  else
   return true;
 }
 
 bool binarySearchIterative(int* array, int startIndex, int lastIndex, int element)
 {
+  while(startIndex<=lastIndex)
+  {
+    int midIndex=(startIndex+lastIndex-1)/2;
+    
+  }
   return true;
 }
 
 int main()
 {
-
-
+  int size;
+  cout<<"\nEnter the number of elements in the array\n";
+  cin>>size;
+  int array[n];
+  cout<<"\nEnter the elements\n";
+  for(int i=0;i<size;i++)
+  {
+    cin>>array[i];
+  }
+  sort(array,array+n);
 
   return 0;
 }
